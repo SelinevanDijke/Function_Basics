9423a60c3d54b541ba2b0537902ccb95ff915b8f
Seline <74591087+SelinevanDijke@users.noreply.github.com>
2021-07-02 18:43:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Writing or defining a function
+const makePizza = function () {
+    console.log("Bak het deeg");
+    console.log("Doe saus op de pizza");
+    console.log("Voeg de toppings toe");
+    console.log("Voeg de kaas toe");
+};
+
+// De functie werkt nog niet omdat de functie nog niet wordt aangeroepen.
+
+// Using or calling  a function
+makePizza();
+makePizza();
+makePizza();
+
+// Writing or defining a function
+const makeSushi = function () {
+    console.log("Bak de rijst en de ingrediënten");
+    console.log("Snijd de ingrediënten");
+    console.log("Voeg alles samen en rol het op in zeewier");
+};
+
+// Using or calling  a function
+makeSushi();
+
+// Working in a restaurant
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
